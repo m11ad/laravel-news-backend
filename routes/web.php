0218341edb95a4b7999dea62b04c113ebeb71d7b
Milad <yekleh@outlook.com>
2022-12-30 23:55:40 +0330

    Added the routes for news

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// /news: Displays a list of news items (handled by the index action of the NewsController)
+Route::get('/news', 'NewsController@index')->name('news.index');
+
+// /news/{newsItem}: Displays a specific news item (handled by the show action of the NewsController)
+Route::get('/news/{newsItem}', 'NewsController@show')->name('news.show');
+
+// /news: Creates a new news item (handled by the store action of the NewsController)
+Route::post('/news', 'NewsController@store')->name('news.store');
+
+// /news/{newsItem}: Updates an existing news item (handled by the update action of the NewsController)
+Route::patch('/news/{newsItem}', 'NewsController@update')->name('news.update');
+
+// /news/{newsItem}: Deletes an existing news item (handled by the destroy action of the NewsController)
+Route::delete('/news/{newsItem}', 'NewsController@destroy')->name('news.destroy');
