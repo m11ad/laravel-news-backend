4419f9a945b6599dbb3a6621e5dd459589928ced
Milad <yekleh@outlook.com>
2022-12-30 23:38:08 +0330

    Created the NewsItem model

---
+++ app/Models/NewsItem.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class NewsItem extends Model
+{
+    // Specify the table name for the model
+    protected $table = 'news_items';
+
+    // Specify the fillable fields for the model
+    protected $fillable = [
+        'title',
+        'body',
+        'category_id',
+        'url',
+    ];
+
+    // Define the relationships for the model
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+}
