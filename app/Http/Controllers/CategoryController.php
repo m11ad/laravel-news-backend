86b274ab0b6f6c04f152f58de40db4c99571f0ed
Milad <yekleh@outlook.com>
2023-01-04 21:41:18 +0330

    Wrote the rest of the categories CRUD functions

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Category;
 use App\Models\NewsItem;
+use Illuminate\Support\Facades\Validator;
+
 
 class CategoryController extends Controller
 {
@@ -12,18 +14,71 @@ class CategoryController extends Controller
     public function index()
     {
         $categories = Category::all();
-
-        return response()->json($categories);
+        return response()->json([
+            'categories' => $categories
+        ], 200);
     }
-
+    
     public function show($id)
     {
         $category = Category::find($id);
-        $news = NewsItem::where('category_id', $id)->get();
-
-        // Return the category and news items as a JSON response
+    
+        if (!$category) {
+            return response()->json([
+                'message' => 'Category not found',
+            ], 404);
+        }
+    
         return response()->json([
-            'category' => $category,
-            'news' => $news,
-        ]);    }
+            'category' => $category
+        ], 200);
+    }
+    
+    public function update(Request $request, $id)
+    {
+        $data = $request->all();
+    
+        $validator = Validator::make($data, [
+            'name' => 'required|max:255',
+        ]);
+    
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Validation failed',
+                'errors' => $validator->errors()
+            ], 422);
+        }
+    
+        $category = Category::find($id);
+    
+        if (!$category) {
+            return response()->json([
+                'message' => 'Category not found',
+            ], 404);
+        }
+    
+        $category->update($data);
+    
+        return response()->json([
+            'message' => 'Category updated successfully',
+            'category' => $category
+        ], 200);
+    }
+    
+    public function destroy($id)
+    {
+        $category = Category::find($id);
+    
+        if (!$category) {
+            return response()->json([
+                'message' => 'Category not found',
+            ], 404);
+        }
+    
+        $category->delete();
+    
+        return response()->json([
+            'message' => 'Category deleted successfully',
+        ], 200);
+    }
 }
