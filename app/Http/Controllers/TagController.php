3e61a446eda2d72ed9400e71b1a426caea0fcf41
Milad <yekleh@outlook.com>
2023-01-05 00:27:47 +0330

    Wrote a TagController

---
+++ app/Http/Controllers/TagController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tag;
+use Illuminate\Http\Request;
+
+
+class TagController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(Tag::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $tag = Tag::create($request->all());
+
+        return response()->json([
+            'message' => 'Tag created successfully',
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $tag = Tag::find($id);
+        if (!$tag) {
+            return response()->json([
+                'error' => 'Tag not found',
+            ], 404);
+        }
+    
+        return response()->json($tag);
+    }
+    
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Tag $tag)
+    {
+        $tag->update($request->all());
+
+        return response()->json([
+            'message' => 'Tag updated successfully',
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Tag $tag)
+    {
+        $tag->delete();
+
+        return response()->json([
+            'message' => 'Tag deleted successfully',
+        ]);
+    }
+}
